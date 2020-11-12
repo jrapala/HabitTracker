7ac685985d6481fb57132a2b2017bcbbc10baef9
Juliette Rapala <jrapala@gmail.com>
2020-11-11 20:09:08 -0600

    feat: add validation when adding item

--- HabitTracker/AddView.swift
+++ HabitTracker/AddView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct AddView: View {
     @State private var name = ""
     @ObservedObject var habits: Habits
+    @State private var showAlert = false
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
@@ -19,9 +22,19 @@ struct AddView: View {
             }
             .navigationBarTitle("Add a New Habit")
             .navigationBarItems(trailing: Button("Save") {
-                let item = HabitItem(name: self.name)
-                self.habits.items.append(item)
-                self.presentationMode.wrappedValue.dismiss()
+                if self.name.count > 0 {
+                    let item = HabitItem(name: self.name)
+                    self.habits.items.append(item)
+                    self.presentationMode.wrappedValue.dismiss()
+                } else {
+                    alertTitle = "Invalid Habit Name"
+                    alertMessage = "Please enter a name for your new habit"
+                    showAlert.toggle()
+                }
+            })
+            .alert(isPresented: $showAlert, content: {
+                Alert(title: Text(alertTitle), message: Text(alertMessage),  dismissButton: .default(Text("OK"))
+                )
             })
         }
     }
